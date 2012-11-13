ef939c29d8681a56997ba003a0684eb974ac8db6
Rishi Ramraj <rishi@bestpremise.com>
2012-11-13 14:15:51 -0500

    Some fixes for mol and seepymol.js.

--- js/seepymol.js
+++ js/seepymol.js
@@ -7,12 +7,15 @@ var GLmol_chemicalobject_handler = function (json, element) {
     var id = 'GLMol-'+IPython.utils.uuid();
     var toinsert = $("<div/>").attr('id',id);
     element.append(toinsert);
+
     var src_id = id + '_src';
-    element.append("<div/>").attr("style", "display: none")
-    .val(json['pdb']);
+    element.append("<div/>").attr("style", "display: none").val(json['pdb']);
+
     var glmol = new GLmol(id, false);
+    /*
     glmol.loadMolecule();
     seepymol.applets[id] = glmol;
+    */
     alert(json['pdb']);
 };
 
