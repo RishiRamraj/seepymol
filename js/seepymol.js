0be0353fc8b3bd993443f449a5ea7334d9b892cf
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2012-11-13 14:30:15 -0500

    Add defineRepresentation to GLmol.

--- js/seepymol.js
+++ js/seepymol.js
@@ -8,9 +8,17 @@ var GLmol_chemicalobject_handler = function (json, element) {
     var toinsert = $("<div/>").attr('id',id);
     element.append(toinsert);
     var src_id = id + '_src';
-    element.append("<div/>").attr("style", "display: none")
+    element.append("<textarea/>").attr("style", "display: none")
     .val(json['pdb']);
-    var glmol = new GLmol(id, false);
+    var glmol = new GLmol(id);
+    glmol.defineRepresentation = function() {
+	var all = this.getAllAtoms();
+	this.colorByAtom(all, {});
+	this.colorByChain(all);
+	var asu = new THREE.Object3D();
+	this.drawCartoon(asu, all, this.curveWidth, this.thickness);
+	this.modelGroup.add(asu);
+    }; 
     glmol.loadMolecule();
     seepymol.applets[id] = glmol;
     alert(json['pdb']);
