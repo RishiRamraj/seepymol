45699e82c7c08649f3aa94fe2a17b9cd4deb454c
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2012-11-13 16:01:51 -0500

    Upgraded Three to r52 and added size to toinsert id.

--- js/seepymol.js
+++ js/seepymol.js
@@ -5,27 +5,16 @@ seepymol = {
 
 var GLmol_chemicalobject_handler = function (json, element) {
     var id = 'GLMol-'+IPython.utils.uuid();
-    var toinsert = $("<div/>").attr('id',id);
+    var toinsert = $("<div/>").attr('id',id).attr('style', "width: 500px; height: 400px; background-color: black;");
     element.append(toinsert);
 
     var src_id = id + '_src';
+    toinsert = $("<textarea/>").attr('id', src_id).val(json['pdb']).hide();
+    element.append(toinsert);
 
-    element.append("<textarea/>").attr("style", "display: none")
-    .val(json['pdb']);
-    var glmol = new GLmol(id);
-    glmol.defineRepresentation = function() {
-	var all = this.getAllAtoms();
-	this.colorByAtom(all, {});
-	this.colorByChain(all);
-	var asu = new THREE.Object3D();
-	this.drawCartoon(asu, all, this.curveWidth, this.thickness);
-	this.modelGroup.add(asu);
-    }; 
-    /*
-    glmol.loadMolecule();
+    var glmol = new GLmol(id, true);
     seepymol.applets[id] = glmol;
-    */
-    alert(json['pdb']);
+    glmol.loadMolecule();
 };
 
 IPython.json_handlers.register_handler('ChemicalObject', GLmol_chemicalobject_handler);
