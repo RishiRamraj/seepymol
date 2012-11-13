aaedc4d384136933a1f32a63e704ce99910e1f09
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2012-11-13 13:59:30 -0500

    seepymol.js now uses GLmol api.

--- js/seepymol.js
+++ js/seepymol.js
@@ -1,9 +1,19 @@
-var jmol_chemicalobject_handler = function (json, element) {
-    var id = 'JsMol-'+IPython.utils.uuid();
+seepymol = {
+    applets: {},
+    loaded: false,
+}
+
+var GLmol_chemicalobject_handler = function (json, element) {
+    var id = 'GLMol-'+IPython.utils.uuid();
     var toinsert = $("<div/>").attr('id',id);
     element.append(toinsert);
-
+    var src_id = id + '_src';
+    element.append("<div/>").attr("style", "display: none")
+    .val(json['pdb']);
+    var glmol = new GLmol(id, false);
+    glmol.loadMolecule();
+    seepymol.applets[id] = glmol;
     alert(json['pdb']);
 };
 
-IPython.json_handlers.register_handler('ChemicalObject', jmol_chemicalobject_handler);
+IPython.json_handlers.register_handler('ChemicalObject', GLmol_chemicalobject_handler);
